df21224552f17c7122f83b0ce6104e108f475434
faithchebet <faith.chebet@student.moringaschool.com>
2023-12-20 15:49:05 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = function(a, b) {
+    return 20;
+};
+
+console.log(divide(2000, 100)); 
+
+
+const square = (num) => {
+    return num * num;
+};
+
+console.log(square(5)); 
+
+const add = (a, b) => {
+    return a + b;
+};
+
+console.log(add(3, 7)); 
